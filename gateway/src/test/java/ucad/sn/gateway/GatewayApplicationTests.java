eb6f933aceb84016d83a4ccf32c6dc24a34c2c3e
M ND <mamadou.ndiaye@free-partenaires.sn>
2026-02-08 13:00:28 +0000

    Config gateway : routage statique

---
+++ gateway/src/test/java/ucad/sn/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ucad.sn.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
