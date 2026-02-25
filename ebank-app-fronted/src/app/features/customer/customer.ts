ae6cb0aedb2dbe750ea46337f8366fa809c4e675
M ND <mamadou.ndiaye@free-partenaires.sn>
2026-02-25 20:48:08 +0000

    communiquer avec le backend via httpClient. Recuperer  la liste des clients et l'afficher

--- ebank-app-fronted/src/app/features/customer/customer.ts
+++ ebank-app-fronted/src/app/features/customer/customer.ts
@@ -1,11 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import { Customer as CustomerService } from '../../services/customer';
+import { CommonModule } from '@angular/common';
+
+import { Customer as CustomerModel } from '../../models/customer';
 
 @Component({
   selector: 'app-customer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
+  standalone: true,
 })
-export class Customer {
+export class Customer implements OnInit {
 
+
+  customerService : CustomerService = inject(CustomerService) ;
+
+  customers: CustomerModel[] = [];
+  loading : boolean = true;
+  error: string | null = null;
+
+  ngOnInit() {
+    this.loadCustomers();
+  }
+
+  loadCustomers() {
+    this.loading = true;
+    this.error = null;
+    this.customers = [];
+
+      this.customerService.getCustomers().subscribe({
+         next: (data) => {
+           console.log('Réponse reçue :', data);           // ← LOG IMPORTANT
+           console.log('Type de data :', Array.isArray(data) ? 'tableau' : typeof data);
+           this.customers = Array.isArray(data) ? data : [];
+           this.loading = false;
+           console.log('customers mis à jour :', this.customers.length, 'éléments');
+
+         },
+         error: (err) => {
+           this.error = 'Error loading customers: ' + (err.message || err.statusText || 'Veriez le gateway');
+           this.loading = false;
+           console.log(err);
+         },
+        complete: () => {
+           //this.loading = false;
+          console.log('Requête terminée');
+          }
+      });
+  }
 }
---
+++ ebank-app-fronted/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiGatewayUrl: 'https://api.mondomaine.com'   // ou ton domaine prod
+};
