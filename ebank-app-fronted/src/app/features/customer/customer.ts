af94f182ad26696fc170881c05dbc84841eba5b9
M ND <mamadou.ndiaye@free-partenaires.sn>
2026-03-02 22:28:14 +0000

    gestion du customer-detail

--- ebank-app-fronted/src/app/features/customer/customer.ts
+++ ebank-app-fronted/src/app/features/customer/customer.ts
@@ -1,52 +1,31 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, signal} from '@angular/core';
 import { Customer as CustomerService } from '../../services/customer';
-import { CommonModule } from '@angular/common';
-
 import { Customer as CustomerModel } from '../../models/customer';
+import { CommonModule } from '@angular/common';
+import {CustomerDetail} from '../customer-detail/customer-detail';
 
 @Component({
   selector: 'app-customer',
-  imports: [CommonModule],
+  imports: [CommonModule, CustomerDetail],
   templateUrl: './customer.html',
   styleUrl: './customer.css',
   standalone: true,
 })
-export class Customer implements OnInit {
+export class Customer {
 
 
   customerService : CustomerService = inject(CustomerService) ;
+  // Le client actuellement sélectionné pour le détail
+  selectedCustomer = signal<CustomerModel | null>(null);
 
-  customers: CustomerModel[] = [];
-  loading : boolean = true;
-  error: string | null = null;
 
-  ngOnInit() {
-    this.loadCustomers();
+  // Méthode appelée quand on clique sur "Voir"
+  viewCustomer(customer: CustomerModel) {
+    this.selectedCustomer.set(customer);
   }
 
-  loadCustomers() {
-    this.loading = true;
-    this.error = null;
-    this.customers = [];
-
-      this.customerService.getCustomers().subscribe({
-         next: (data) => {
-           console.log('Réponse reçue :', data);           // ← LOG IMPORTANT
-           console.log('Type de data :', Array.isArray(data) ? 'tableau' : typeof data);
-           this.customers = Array.isArray(data) ? data : [];
-           this.loading = false;
-           console.log('customers mis à jour :', this.customers.length, 'éléments');
-
-         },
-         error: (err) => {
-           this.error = 'Error loading customers: ' + (err.message || err.statusText || 'Veriez le gateway');
-           this.loading = false;
-           console.log(err);
-         },
-        complete: () => {
-           //this.loading = false;
-          console.log('Requête terminée');
-          }
-      });
+  // Fermer la modale
+  closeDetail() {
+    this.selectedCustomer.set(null);
   }
 }
